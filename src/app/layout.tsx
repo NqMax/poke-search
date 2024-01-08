0aa2ba9d4aff129146710a6d233b5d6fd7ab9095
Max <111196993+NqMax@users.noreply.github.com>
2024-01-08 17:07:53 -0600

    Added font configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,30 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter, Roboto_Mono } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ["latin"],
+  variable: "--font-roboto-mono",
+});
 
 export const metadata: Metadata = {
-  title: 'Pokémon Search',
-  description: 'Search, discover, and learn about your favorite Pokémon. Find detailed information, including stats, abilities, types, and evolutions.',
-}
+  title: "Pokémon Search",
+  description:
+    "Search, discover, and learn about your favorite Pokémon. Find detailed information, including stats, abilities, types, and evolutions.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} ${roboto_mono.variable} font-inter`}>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
