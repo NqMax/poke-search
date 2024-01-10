9f1a5f8ff858a747e35a74863652c3600e1b5489
Max <111196993+NqMax@users.noreply.github.com>
2024-01-09 18:02:14 -0600

    Modified pokemon card and its fallback to apply a holographic effect

--- src/components/fallbacks.tsx
+++ src/components/fallbacks.tsx
@@ -1,8 +1,28 @@
+import Image from "next/image";
+import PikachuSilhouette from "/public/pikachu-silhouette.png";
+
+// export function PokemonCardFallback() {
+//   return (
+// <div className="flex h-40 w-40 animate-pulse flex-col items-center justify-center rounded-md bg-slate-200 p-2">
+//   <Image
+//     src={PikachuSilhouette}
+//     width={80}
+//     height={80}
+//     alt="pokemon placeholder"
+//   />
+// </div>
+//   );
+// }
 export function PokemonCardFallback() {
   return (
-    <div className="flex h-40 w-40 animate-pulse flex-col items-center justify-center rounded-md bg-slate-200  p-2">
-      <div className="h-20 w-20 rounded bg-slate-400" />
-      <div className="mt-2 h-4 w-20 rounded-md bg-slate-400" />
+    <div className="relative flex h-40 w-40 animate-pulse flex-col items-center justify-center rounded-md bg-neutral-600 p-2">
+      <Image
+        src={PikachuSilhouette}
+        width={100}
+        height={100}
+        alt="pokemon placeholder"
+      />
+      <div className="cardShine absolute h-40 w-40"></div>
     </div>
   );
 }
--- src/components/pokemonCard.tsx
+++ src/components/pokemonCard.tsx
@@ -1,27 +1,27 @@
 import Image from "next/image";
 import { getPokemonData } from "@/lib/utils";
 import { getBase64 } from "@/lib/util";
+import PikachuSilhouette from "/public/pikachu-silhouette.png";
 
 export async function PokemonCard({ name }: { name: string }) {
   const pokemonData = await getPokemonData(
     `https://pokeapi.co/api/v2/pokemon/${name}`,
   );
 
-  const blurredPhoto = await getBase64(
-    pokemonData.sprites.other["official-artwork"].front_default,
-  );
+  const blurredPhoto = await getBase64(pokemonData.sprites.front_default);
 
   return (
-    <div className="flex h-40 w-40 flex-col items-center justify-center rounded-md bg-slate-200 p-2">
+    <div className="relative flex h-40 w-40 flex-col items-center justify-center rounded-md bg-neutral-600 p-2">
       <Image
-        src={pokemonData.sprites.other["official-artwork"].front_default}
+        src={pokemonData.sprites.front_default || PikachuSilhouette}
         alt={`${name} pokemon artwork`}
-        width={80}
-        height={80}
+        width={100}
+        height={100}
         placeholder="blur"
         blurDataURL={blurredPhoto}
       />
-      <div className="font-roboto">{name}</div>
+      <div className="text-center">{name}</div>
+      <div className="cardShine absolute h-40 w-40"></div>
     </div>
   );
 }
