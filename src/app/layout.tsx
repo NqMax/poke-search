4b2c142d6f5d03119603f0b5a5e859aa97c006b4
Max <111196993+NqMax@users.noreply.github.com>
2024-01-10 17:46:40 -0600

    Removed export

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
-export const roboto_mono = Roboto_Mono({
+const roboto_mono = Roboto_Mono({
   subsets: ["latin"],
   variable: "--font-roboto-mono",
 });
