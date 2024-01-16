b64263e3d88c0c715e06efa89337987d869dcdb5
Max <111196993+NqMax@users.noreply.github.com>
2024-01-15 22:45:28 -0600

    Modified function constant

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,10 +1,11 @@
 import { pokemon } from "@/lib/pokemon";
 
 function getRandomPokemon() {
-  return pokemon[Math.floor(Math.random() * 1300)];
+  return pokemon[Math.floor(Math.random() * 1282)];
 }
 
 export function getRandomPokemonArray() {
+  console.log(pokemon.length)
   return Array(7)
     .fill(0)
     .map(() => {
