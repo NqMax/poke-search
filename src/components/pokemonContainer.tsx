9a36e16622d26e83253be616d234b605bf358b46
Max <111196993+NqMax@users.noreply.github.com>
2024-01-17 16:43:24 -0600

    Modified container for infinite scrolling to work along the search feature

--- src/components/pokemonContainer.tsx
+++ src/components/pokemonContainer.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect, useRef } from "react";
 import { PokemonCard } from "@/components/pokemonCard";
 import { PokemonCardFallback } from "@/components/fallbacks";
-import { getPokemon, getPokemonByName } from "@/lib/serverActions";
+import { getPokemon } from "@/lib/serverActions";
 import { Loader } from "@/components/loader";
 import type { Pokemon } from "@/lib/types";
 
@@ -14,55 +14,43 @@ export function PokemonContainer({
   };
 }) {
   const query = searchParams?.query || "";
-  const [pokemonData, setPokemonData] = useState<Pokemon>([]);
-  const [offset, setOffset] = useState(0);
-  const [dataExists, setDataExists] = useState(false);
+  const [pokemonData, setPokemonData] = useState<Pokemon>({
+    next: null,
+    pokemon: [],
+  });
   const [loading, setLoading] = useState(false);
   const containerRef = useRef(null);
 
   useEffect(() => {
     async function fetchData() {
-      const data = await getPokemonByName(query, offset);
-      setPokemonData((prev) => [...prev, ...data]);
-      setLoading(false);
-      if (data.length === 0) {
-        setDataExists(false);
-      } else {
-        setDataExists(true);
-      }
-    }
-    fetchData();
-  }, [offset]);
-
-  useEffect(() => {
-    setOffset(0);
-    async function fetchData() {
-      const data = await getPokemonByName(query, 0);
+      const data = await getPokemon(query, 0);
       setPokemonData(data);
-      setLoading(false);
-      if (data.length === 0) {
-        setDataExists(false);
-      } else {
-        setDataExists(true);
-      }
     }
     fetchData();
   }, [query]);
 
   useEffect(() => {
+    async function fetchData() {
+      const data = await getPokemon(query, pokemonData.next);
+      setPokemonData({
+        ...data,
+        pokemon: [...pokemonData.pokemon, ...data.pokemon],
+      });
+    }
+
     let observer = new IntersectionObserver(
       (entries, observer) => {
         if (entries[0].isIntersecting) {
-          setLoading(true);
-          setOffset((prev) => prev + 50);
+          fetchData();
         }
       },
       {
-        rootMargin: "0px",
         threshold: 0.5,
       },
     );
-    dataExists && observer.observe(containerRef.current!);
+
+    pokemonData.next && observer.observe(containerRef.current!);
+
     return () => observer.disconnect();
   }, [pokemonData]);
 
@@ -70,17 +58,22 @@ export function PokemonContainer({
     <>
       {/* <div className="flex flex-wrap gap-x-2 gap-y-2 place-content-center"> */}
       <div className="relative my-3 grid grid-cols-[repeat(auto-fill,_10rem)] place-content-center gap-x-2 gap-y-2">
-        {pokemonData.map((pokemon, index) => (
-          <PokemonCard key={index} name={pokemon.name} src={pokemon.sprite} />
+        {pokemonData.pokemon.map((pokemon) => (
+          <PokemonCard
+            key={pokemon.name}
+            name={pokemon.name}
+            src={pokemon.sprite}
+          />
         ))}
-        {dataExists && (
+        {pokemonData.next && (
           <div
-            className="absolute bottom-36 h-12 w-full"
+            className="absolute bottom-32 h-12 w-full bg-white"
             ref={containerRef}
-          ></div>
+          />
         )}
       </div>
       {loading && <Loader />}
+      <Loader />
     </>
   );
 }
