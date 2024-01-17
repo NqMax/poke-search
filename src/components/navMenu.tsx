5e9d6df7879d46ba9ce587185f07595b52e86982
Max <111196993+NqMax@users.noreply.github.com>
2024-01-16 22:21:00 -0600

    Modified NavMenu

--- src/components/navMenu.tsx
+++ src/components/navMenu.tsx
@@ -13,17 +13,30 @@ import {
 import Link from "next/link";
 import { MdOutlineCatchingPokemon } from "react-icons/md";
 
+function NavItem({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <li className="flex flex-col justify-center gap-y-1 rounded-md p-3 transition-colors hover:bg-neutral-800">
+      <div className="text-sm font-medium leading-none">{title}</div>
+      <p className="line-clamp-2 text-sm leading-tight text-neutral-400">
+        {children}
+      </p>
+    </li>
+  );
+}
+
 export function NavMenu() {
   return (
     <NavigationMenu className="hidden sm:flex">
       <NavigationMenuList>
         <NavigationMenuItem>
           <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
-            <Link
-              href="/pokemon"
-            >
-              Pokemon
-            </Link>
+            <Link href="/pokemon">Pokémon</Link>
           </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
@@ -40,17 +53,21 @@ export function NavMenu() {
         <NavigationMenuItem>
           <NavigationMenuTrigger>About</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <div className="flex p-6">
-              <div className="flex w-48 flex-col gap-y-2 rounded-md bg-neutral-800 p-6">
+            <ul className="grid w-[30rem] grid-cols-[0.75fr_1fr] grid-rows-3 gap-3 p-6">
+              <li className="row-span-3 flex flex-col justify-end rounded-md gap-y-px bg-neutral-800/80 p-6">
                 <MdOutlineCatchingPokemon className="text-red-500" />
-                <div className="text-lg font-medium">Poke/Search</div>
+                <div className="text-lg font-medium mt-3">Poké Search</div>
                 <p className="text-sm leading-tight text-neutral-400 ">
                   Search, discover, and learn about your favorite Pokémon. Find
-                  detailed information, including stats, abilities, types, and
-                  evolutions.
+                  stats, abilities, types, evolutions and more. Based on the
+                  amazing PokéAPI.
                 </p>
-              </div>
-            </div>
+              </li>
+              <NavItem title="PokéAPI">
+                All the Pokémon data you will ever need in one place, easily
+                accessible through a modern free open-source RESTful API.
+              </NavItem>
+            </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
       </NavigationMenuList>
