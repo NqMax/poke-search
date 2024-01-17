7cb2084ca0245612ddc6bd9f4dbdba0897379ee6
Max <111196993+NqMax@users.noreply.github.com>
2024-01-17 16:44:23 -0600

    Modified fetch pokemon function to include the offset in its return value, type adjusted accordingly

--- src/lib/serverActions.ts
+++ src/lib/serverActions.ts
@@ -1,19 +1,27 @@
 "use server";
-import { pokemon } from "./pokemon";
+import { pokemonList } from "./pokemon";
 
-export async function getPokemon(offset: number) {
-  if (offset == 0) {
-    return pokemon.slice(0, 50);
+export async function getPokemon(query: string, offset: number | null) {
+  if (offset === null) {
+    return {
+      next: null,
+      pokemon: [],
+    };
   }
-  return pokemon.slice(offset, offset + 50);
-}
 
-export async function getPokemonByName(query: string, offset: number) {
-  const filteredPokemon = pokemon.filter((poke) =>
-    poke.name.toLowerCase().includes(query.toLowerCase()),
-  );
-  if (offset == 0) {
-    return filteredPokemon.slice(0, 50);
-  }
-  return filteredPokemon.slice(offset, offset + 50);
+  const filteredPokemon =
+    query === ""
+      ? pokemonList
+      : pokemonList.filter((poke) =>
+          poke.name.toLowerCase().includes(query.toLowerCase()),
+        );
+
+  const pokemon = filteredPokemon.slice(offset, offset + 50);
+  return {
+    next:
+      filteredPokemon.slice(offset + 50, offset + 100).length === 0
+        ? null
+        : offset + 50,
+    pokemon: pokemon,
+  };
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,26 +1,29 @@
-export type Pokemon = (
-  | {
-      name: string;
-      id: number;
-      sprite: string;
-      types: {
-        slot: number;
-        type: {
-          name: string;
-          url: string;
-        };
-      }[];
-    }
-  | {
-      name: string;
-      id: number;
-      sprite: null;
-      types: {
-        slot: number;
-        type: {
-          name: string;
-          url: string;
-        };
-      }[];
-    }
-)[];
+export type Pokemon = {
+  next: number | null;
+  pokemon: (
+    | {
+        name: string;
+        id: number;
+        sprite: string;
+        types: {
+          slot: number;
+          type: {
+            name: string;
+            url: string;
+          };
+        }[];
+      }
+    | {
+        name: string;
+        id: number;
+        sprite: null;
+        types: {
+          slot: number;
+          type: {
+            name: string;
+            url: string;
+          };
+        }[];
+      }
+  )[];
+};
