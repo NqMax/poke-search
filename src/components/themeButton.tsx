d3446064ba1b96a8645036037271635d0a70e829
Max <111196993+NqMax@users.noreply.github.com>
2024-01-23 11:54:28 -0600

    Modified Icon and navigation link

--- src/components/themeButton.tsx
+++ src/components/themeButton.tsx
@@ -1,10 +1,17 @@
+import Link from "next/link";
 import { RxMoon } from "react-icons/rx";
 import { Button } from "@/components/ui/button";
-
+import { FaGithub } from "react-icons/fa";
 export function ThemeButton() {
   return (
-    <Button variant="ghost" size="icon">
-      <RxMoon className="h-5 w-5"/>
+    <Button variant="ghost" size="icon" asChild>
+      <Link
+        href="https://github.com/NqMax/poke-search"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaGithub className="h-5 w-5" />
+      </Link>
     </Button>
   );
 }
